281a8935a6bda58765ca0d23d625faa74d2134a0
cyphr0st <iamaaronhodge@gmail.com>
2020-11-22 19:47:40 -0500

    still organizing

---
+++ stage/services/instances.tf
@@ -0,0 +1,5 @@
+#Fetching Linux AMI ID using SSM
+data "aws_ssm_parameter" "linuxAmi" {
+  provider = aws.region-master
+  name     = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
