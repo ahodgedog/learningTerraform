27ed784833a9e2ee88e2a01ca645ef7234482e25
cyphr0st <iamaaronhodge@gmail.com>
2020-11-24 16:09:51 -0500

    user_data parameter added

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -17,3 +17,14 @@ data "template_file" "user_data" {
     db_port = "${data.terraform_remote_state.db.port}"
   }
 }
+
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-40d28157"
+  instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
+  user_data = "${data.template_file.user_data.rendered}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
